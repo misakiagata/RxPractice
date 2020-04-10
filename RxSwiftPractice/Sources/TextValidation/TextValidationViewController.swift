61e760a43602411233230c3478729694cbc72d21
Misaki Agata <p17004am@gm.tsuda.ac.jp>
2020-04-10 14:25:35 +0900

    fix textValidation

--- RxSwiftPractice/Sources/TextValidation/TextValidationViewController.swift
+++ RxSwiftPractice/Sources/TextValidation/TextValidationViewController.swift
@@ -20,37 +20,55 @@ class TextValidationViewController: UIViewController {
     
     let maxNameFieldSize = 10
     let maxAddressFieldSize = 50
-    let limitText: (Int) -> String = {
-        return "あと\($0) 文字"
-    }
+    //    let limitText: (Int) -> String = {
+    //        return "あと\($0) 文字"
+    //    }
     
     private var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //        nameTextField.rx.text
+        //            .map { [weak self] text -> String? in
+        //                guard let text = text else { return nil }
+        //                guard let maxNameFieldSize = self?.maxNameFieldSize else { return nil }
+        //                let limitCount = maxNameFieldSize - text.count
+        //                return self?.limitText(limitCount)
+        //        }
+        //        .filterNil()
+        //        .observeOn(MainScheduler.instance)
+        //        .bind(to: nameLabel.rx.text)
+        //        .disposed(by: disposeBag)
+        //
+        //        addressTextField.rx.text
+        //            .map { [weak self] text -> String? in
+        //                guard let text = text else { return nil }
+        //                guard let maxAddressFieldSize = self?.maxAddressFieldSize else { return nil }
+        //                let limitCount = maxAddressFieldSize - text.count
+        //                return self?.limitText(limitCount)
+        //        }
+        //        .filterNil()
+        //        .observeOn(MainScheduler.instance)
+        //        .bind(to: addressLabel.rx.text)
+        //        .disposed(by: disposeBag)
+        
         nameTextField.rx.text
-            .map { [weak self] text -> String? in
+            .map { text -> String? in
                 guard let text = text else { return nil }
-                guard let maxNameFieldSize = self?.maxNameFieldSize else { return nil }
-                let limitCount = maxNameFieldSize - text.count
-                return self?.limitText(limitCount)
-        }
-        .filterNil()
-        .observeOn(MainScheduler.instance)
-        .bind(to: nameLabel.rx.text)
-        .disposed(by: disposeBag)
+                let limitCount = self.maxNameFieldSize - text.count
+                return "あと\(limitCount)文字"
+            }
+            .bind(to: nameLabel.rx.text)
+            .disposed(by: disposeBag)
         
         addressTextField.rx.text
-            .map { [weak self] text -> String? in
+            .map { text -> String? in
                 guard let text = text else { return nil }
-                guard let maxAddressFieldSize = self?.maxAddressFieldSize else { return nil }
-                let limitCount = maxAddressFieldSize - text.count
-                return self?.limitText(limitCount)
-        }
-        .filterNil()
-        .observeOn(MainScheduler.instance)
-        .bind(to: addressLabel.rx.text)
-        .disposed(by: disposeBag)
+                let limitCount = self.maxAddressFieldSize - text.count
+                return "あと\(limitCount)文字"
+            }
+            .bind(to: addressLabel.rx.text)
+            .disposed(by: disposeBag)
     }
 }
